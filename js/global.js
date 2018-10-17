67cd12c98a9aed6b91ad205759e422ba958b9c06
Leonardo Herman <leo@decentraland.org>
2018-10-17 12:19:55 -0300

    add timestamp to scripts and stylesheets avoid caching issues

--- js/global.js
+++ js/global.js
@@ -255,4 +255,23 @@ $(function() {
       showSearchResults()
     })
   })
+
+  const $sidebar = $(".sidebar")
+
+  $sidebar.find(".dropdown").click(function(event) {
+    event.preventDefault()
+    $sidebar.addClass("open")
+  })
+
+  $sidebar.find(".close, .sidebar-overlay").click(function(event) {
+    event.preventDefault()
+    $sidebar.removeClass("open")
+  })
+
+  $sidebar.find(".toggle-item").click(function(event) {
+    event.preventDefault()
+    const data = $(event.target).attr("data-toggle")
+    $('.toggle[data-id!="' + data + '"]').hide("fast")
+    $('.toggle[data-id="' + data + '"]').toggle("fast")
+  })
 })
