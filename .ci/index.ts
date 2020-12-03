d756ec5412db5d6285e5148fd94ded61f7e81c68
Fede <i@2fd.me>
2020-12-03 11:01:11 -0300

    chore: add domain alias

--- .ci/index.ts
+++ .ci/index.ts
@@ -1,9 +1,11 @@
 import { envTLD } from "dcl-ops-lib/domain";
 import { buildStatic } from "dcl-ops-lib/buildStatic";
+import { env } from "process";
 
 async function main() {
   const page = buildStatic({
     domain: `docs.decentraland.${envTLD}`,
+    additionalDomains: envTLD === 'co' ? [ 'docs.decentraland.org' ] : [],
     path: '/',
   });
 
