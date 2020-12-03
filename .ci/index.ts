ce6097cfeee29e96d914b95c97699354f1bae334
Fede <i@2fd.me>
2020-12-03 14:11:13 -0300

    chore: fix arn

--- .ci/index.ts
+++ .ci/index.ts
@@ -3,12 +3,8 @@ import { buildStatic } from "dcl-ops-lib/buildStatic";
 
 async function main() {
 
-  const domain = envTLD === 'co' ? 'docs.decentraland.org' : `docs.decentraland.${envTLD}`
-  const certificateArn = envTLD === 'co' ? '	arn:aws:acm:us-east-1:619079673649:certificate/64b31250-8656-4bf3-ad74-dc5def47476d' : undefined
-
   const page = buildStatic({
-    domain,
-    certificateArn,
+    domain: `docs.decentraland.${envTLD}`,
     defaultPath: '404-error/index.html',
     path: '/',
   });
