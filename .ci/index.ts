7af49f7c9691de89e798ce874364f1696c20db63
Fede <i@2fd.me>
2020-12-03 11:59:52 -0300

    chore: fix certificateArn

--- .ci/index.ts
+++ .ci/index.ts
@@ -3,10 +3,13 @@ import { buildStatic } from "dcl-ops-lib/buildStatic";
 
 async function main() {
 
-  const tld = envTLD === 'co' ? 'org' : envTLD
+  const domain = envTLD === 'co' ? 'docs.decentraland.org' : `docs.decentraland.${envTLD}`
+  const certificateArn = envTLD === 'co' ? '	arn:aws:acm:us-east-1:619079673649:certificate/64b31250-8656-4bf3-ad74-dc5def47476d' : undefined
 
   const page = buildStatic({
-    domain: `docs.decentraland.${tld}`,
+    domain,
+    certificateArn,
+    defaultPath: '404-error/index.html',
     path: '/',
   });
 
