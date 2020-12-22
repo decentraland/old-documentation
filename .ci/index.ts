bc565cda2c6b3b37bb611f034b3a2600c471d943
Fede <i@2fd.me>
2020-12-22 13:11:03 -0300

    chore: set certificateArn

--- .ci/index.ts
+++ .ci/index.ts
@@ -3,8 +3,12 @@ import { buildStatic } from "dcl-ops-lib/buildStatic";
 
 async function main() {
 
+  const domain = envTLD === 'co' ? 'docs.decentraland.org' : `docs.decentraland.${envTLD}`
+  const certificateArn = envTLD === 'co' ? 'arn:aws:acm:us-east-1:619079673649:certificate/64b31250-8656-4bf3-ad74-dc5def47476d' : undefined
+
   const page = buildStatic({
-    domain: `docs.decentraland.${envTLD === 'co' ? 'org' : envTLD}`,
+    domain,
+    certificateArn,
     defaultPath: '404-error/index.html',
     path: '/',
   });
